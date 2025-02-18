8646fd70e6c2a766ae4c664f34fb87d3ad520af9
ivanrodriiguez04 <103932821+ivanrodriiguez04@users..noreplygithub.com>
2025-02-18 13:45:51 +0100

    Modificacion para angular

--- src/main/java/Api/proyectoFinalDWSDIW/configuracion/WebConfig.java
+++ src/main/java/Api/proyectoFinalDWSDIW/configuracion/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // Aplica las reglas a todas las rutas bajo "/api/**"
-                .allowedOrigins("http://localhost:8080") // Permite solicitudes desde el frontend en localhost:8080
+                .allowedOrigins("http://localhost:8080","http://localhost:4200") // Permite solicitudes desde el frontend en localhost:8080
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos HTTP permitidos
                 .allowedHeaders("*") // Permite todos los encabezados en la solicitud
                 .allowCredentials(true); // Permite el uso de cookies o credenciales en las solicitudes
