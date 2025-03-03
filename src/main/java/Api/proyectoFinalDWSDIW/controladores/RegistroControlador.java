2f4e7de884a9ebae5b5b5863e43bdab0ce6d1131
ivanrodriiguez04 <103932821+ivanrodriiguez04@users..noreplygithub.com>
2025-03-03 14:44:18 +0100

    prueba

--- src/main/java/Api/proyectoFinalDWSDIW/controladores/RegistroControlador.java
+++ src/main/java/Api/proyectoFinalDWSDIW/controladores/RegistroControlador.java
@@ -1,11 +1,9 @@
 package Api.proyectoFinalDWSDIW.controladores;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,22 +13,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import Api.proyectoFinalDWSDIW.daos.RegistroTemporalDao;
-import Api.proyectoFinalDWSDIW.daos.UsuarioDao;
 import Api.proyectoFinalDWSDIW.dtos.RegistroDto;
-import Api.proyectoFinalDWSDIW.repositorios.RegistroTemporalRepositorio;
-import Api.proyectoFinalDWSDIW.repositorios.UsuarioRepositorio;
 import Api.proyectoFinalDWSDIW.servicios.EmailServicio;
 import Api.proyectoFinalDWSDIW.servicios.UsuarioServicio;
 
 @RestController
 @RequestMapping("/api/registro")
 public class RegistroControlador {
-	@Autowired
-    private UsuarioRepositorio usuarioRepositorio;
-	@Autowired
-	private RegistroTemporalRepositorio registroTemporalRepositorio;
-	@Autowired
+@Autowired
 	private EmailServicio emailServicio;
     @Autowired
     private UsuarioServicio usuarioServicio;
@@ -62,13 +52,18 @@ public class RegistroControlador {
         }
     }
     @GetMapping("/confirmar")
-    public ResponseEntity<String> confirmarUsuario(@RequestParam("token") String token) {
+    public ResponseEntity<Object> confirmarUsuario(@RequestParam("token") String token) {
         boolean confirmado = usuarioServicio.confirmarRegistro(token);
 
         if (confirmado) {
-            return ResponseEntity.ok("Cuenta confirmada. Inicia sesión en http://localhost:4200/login");
+            return ResponseEntity.status(302)
+                    .header("Location", "http://localhost:4200/login")  // Nueva URL del login en Angular
+                    .build();
         } else {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Token inválido o expirado.");
+            return ResponseEntity.status(302)
+                    .header("Location", "http://localhost:4200/error")  // Redirigir a una página de error si falla
+                    .build();
         }
     }
+
 }
